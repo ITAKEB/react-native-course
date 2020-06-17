a59aed9e7c5e1f07301375620333637958907519
Tomas <tomascf123@gmail.com>
2020-06-17 17:38:03 -0500

    Primer video uwu Aquí aprendí los componentes de Text,View Image, Buton y un poco de Flexbox , aunque aun no me queda muy claro. Hice mi primera aplicación en native xd

--- App.js
+++ App.js
@@ -1,19 +1,99 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import {Text, View, StyleSheet, Image, Button, Alert, ImageBackground} from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends Component{
+
+  saludo = () =>{ Alert.alert("Hola guapo")}
+  
+  render(){
+    
+    return(
+      <View style={styles.container}>
+
+        
+        
+          <View style={styles.header}> 
+            <View style={styles.headerLeft}>
+              <Image source={ require("./assets/ola.png")} style={styles.logo}/>
+            </View>
+    
+            <View style={styles.headerRight}>
+              <Button title="Login" onPress={this.saludo} style={styles.boton}/>
+            </View>  
+          </View>
+
+
+        <View style={styles.body}>
+          <Text style={styles.negrita}>BIENVENIDO A MI PRIMERA APP EN NATIVE UWU</Text>
+        </View>
+
+        <View style={styles.perfil}>
+          <View>
+            <Text style={styles.negrita}>
+              Tomasuwu
+            </Text>
+          </View>
+          <View>
+            <Text  style={styles.negrita}>
+              HolaGuapo
+            </Text>
+          </View>
+            
+          <View>
+            <Image source={{uri: "https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg"}} style={styles.logoP}/>
+          </View>
+        </View>
+
+      </View>
+      
+    
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container : {
+    flex : 1,
+    backgroundColor: "green",
+    flexDirection: "column"
   },
-});
+  negrita:{
+    color:"white",
+    fontWeight: "bold"
+  },  
+  header:{
+    flex:0.13,
+    flexDirection: "row",
+    marginTop: 40
+  },  
+  headerLeft:{
+    flex:1
+  },
+  headerRight:{
+    flex:0.4,
+  },
+  body:{
+    flex:1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  perfil:{
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },  
+  logo:{
+    width:80,
+    height: 80,
+    borderRadius: 200,
+    resizeMode: "contain",
+  },
+  logoP:{
+    width:80,
+    height: 80,
+    borderRadius: 200,
+    resizeMode: "contain", 
+  }
+})
+export default App
